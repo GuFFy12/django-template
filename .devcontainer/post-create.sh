1cba849cb88c1b10a16406ae08d9f496ec68b863
GuFFy12 <jokers_hello_0d@icloud.com>
2026-06-03 14:45:43 +0500

    .

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo chown vscode .venv
+
+# Команды которые запустятся после первого запуска devcontainer.
+
+uv sync --frozen
+uv run dprint output-resolved-config > /dev/null
+uv run lefthook install
+
+uv run python manage.py migrate
+uv run python manage.py shell -c "from django.contrib.auth import get_user_model as g; u,_=g().objects.get_or_create(username='admin'); u.set_password('admin'); u.is_superuser=u.is_staff=True; u.save()"
